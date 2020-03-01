633ee0d09143aecc342e98f479d485399111732f
qiaoqiao.liu <qiaoqiao.liu@juphoon.com.cn>
2020-03-02 00:24:39 +0800

    a

--- MakeMoney/Classes/Model/HomeItem.m
+++ MakeMoney/Classes/Model/HomeItem.m
@@ -25,21 +25,6 @@
              };
 }
 
-- (void)setImg:(NSString *)img
-{
-    _img = img;
-    __weak __typeof(self) weakSelf = self;
-
-   
-//    [[SDWebImageDownloader sharedDownloader] downloadImageWithURL:[NSURL URLWithString:img] options:SDWebImageDownloaderHighPriority progress:^(NSInteger receivedSize, NSInteger expectedSize) {
-//        NSLog(@"receivedSize:%zd",receivedSize);
-//    } completed:^(UIImage *image, NSData *data, NSError *error, BOOL finished) {
-//        if (image) {
-//            weakSelf.imageSize = image.size;
-//        }
-//    }];
-}
-
 @end
 
 @implementation HomeVideoList
